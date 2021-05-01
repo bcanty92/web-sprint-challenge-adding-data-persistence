acbf174908a6b3ab924e9eea22c93c2af0c80ef1
BrittanyCanty <bcanty92@gmail.com>
2021-05-01 00:56:03 -0500

    initial

--- api/server.js
+++ api/server.js
@@ -1,1 +1,15 @@
 // build your server here and require it from index.js
+const express = require('express');
+const helmet = require('helmet');
+
+const ProjectRouter = require('../projects/project-router')
+
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+//server api
+server.use('/api', ProjectRouter);
+
+module.exports = server;
